e933698b2a011bb54cea820b24906daa37099837
Anzheeela <roza.harafytdinova555@gmail.com>
2023-03-14 01:24:38 +0300

    4 alg

--- src/algtest.cpp
+++ src/algtest.cpp
@@ -10,10 +10,13 @@ bool compare(int a, int b)
 int main()
 {
     int arr[] = {2, 4, 1, 7, 3, -8, 3, -4, 2, -2, 1, 4, 2, -3, -8, 6, 8, 5, 8, -3, 0};
-    bubblesort<int>(arr, arr + 21, compare);
+    SelectionSort<int>(arr, arr + 21, compare);
     for (int i = 0; i < 21; i++)
     {
         std::cout<<arr[i]<<" ";
     }
     std::cout<<std::endl;
 }
+
+
+
