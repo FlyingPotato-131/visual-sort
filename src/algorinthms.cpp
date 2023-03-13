cab995b8ba564afb1f2981e22a80ec76514aeea7
Anzheeela <roza.harafytdinova555@gmail.com>
2023-03-13 23:19:30 +0300

    first alg

--- src/algorinthms.cpp
+++ src/algorinthms.cpp
@@ -1,11 +1,20 @@
-template <typename T, typename cmp = std::less<T>>
-void bubblesort(T*begin, T*end, cmp comp = std::less<T>{})
+template <typename T>
+void swap(T *a, T *b) 
+{
+    T c = *a;
+    *a = *b;
+    *b = c;
+}
+
+
+template <typename T>
+void bubblesort(T*begin, T*end, bool(*compare)(const T, const T))
 {
     for (size_t i = 0; i + 1 < end - begin; ++i) 
     {
         for (size_t j = 0; j + 1 < end - begin - i; ++j) 
         {
-            if (begin[j + 1] < begin[j]) 
+            if (compare(begin[j + 1], begin[j])) 
             {
                 swap(begin + j, begin + j + 1);
             }
