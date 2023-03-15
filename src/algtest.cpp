496542b4cda789e1a753a054506e794c2b2bb4ef
Anzheeela <roza.harafytdinova555@gmail.com>
2023-03-15 23:50:19 +0300

    try

--- src/algtest.cpp
+++ src/algtest.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include "algorinthms.cpp"
+#include "mergesort.cpp"
 
 
 bool compare(int a, int b)
 {
-    return a > b;
+    return a < b;
 }
 
 int main()
 {
     int arr[] = {2, 4, 1, 7, 3, -8, 3, -4, 2, -2, 1, 4, 2, -3, -8, 6, 8, 5, 8, -3, 0};
-    SelectionSort<int>(arr, arr + 21, compare);
+    mergeSort<int>(arr, arr + 21, compare);
     for (int i = 0; i < 21; i++)
     {
         std::cout<<arr[i]<<" ";
     }
     std::cout<<std::endl;
 }
-
-
 
