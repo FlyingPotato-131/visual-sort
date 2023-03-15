675197fbbadac08cc7c2ede6cc940581d3aa1184
Anzheeela <roza.harafytdinova555@gmail.com>
2023-03-16 01:50:09 +0300

    timsort

--- src/algtest.cpp
+++ src/algtest.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include "mergesort.cpp"
+#include "timsort.cpp"
 
 
 bool compare(int a, int b)
 {
-    return a < b;
+    return a > b;
 }
 
 int main()
 {
-    int arr[] = {2, 4, 1, 7, 3, -8, 3, -4, 2, -2, 1, 4, 2, -3, -8, 6, 8, 5, 8, -3, 0};
-    mergeSort<int>(arr, arr + 21, compare);
-    for (int i = 0; i < 21; i++)
+    int arr[] = {3, 6, 2, 6, 2, 7, 4, 6, 7, 1};
+    timSort<int>(arr, arr + 10, compare);
+    for (int i = 0; i < 10; i++)
     {
         std::cout<<arr[i]<<" ";
     }
